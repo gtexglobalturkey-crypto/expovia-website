2179eae45c071923bad9dab59caa5df079303b27
Ahmet Burak Eryıldırım <gtexglobalturkey@gmail.com>
2026-07-23 03:55:46 +0300

    fix: correct casing of Industries import path for Linux builds

--- src/pages/Industries.jsx
+++ src/pages/Industries.jsx
@@ -1,9 +1,9 @@
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 
-import IndustriesHero from "../components/industries/IndustriesHero";
-import IndustriesGrid from "../components/industries/IndustriesGrid";
-import IndustryCTA from "../components/industries/IndustryCTA";
+import IndustriesHero from "../components/Industries/IndustriesHero";
+import IndustriesGrid from "../components/Industries/IndustriesGrid";
+import IndustryCTA from "../components/Industries/IndustryCTA";
 
 function Industries() {
   return (
